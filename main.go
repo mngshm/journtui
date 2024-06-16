15a9ad4df498ea34fe2622c422e4d5e9797cd6c1
mangeshrex <mangeshrex@tutanota.com>
2024-06-17 01:11:23 +0530

    add: search bar and type functionality

--- main.go
+++ main.go
@@ -8,22 +8,26 @@ import (
 var app = tview.NewApplication()
 var mainBox = tview.NewBox().
   SetBorder(true).
-  SetTitle("Journalctl but TUI")
+  SetTitle("service logs")
 
-var searchBox = tview.NewBox().
-  SetBorder(true).
-  SetTitle("Search service")
-  
+var searchBox = tview.NewInputField() 
 
 func main(){
-  mainBox.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+  flex := tview.NewFlex().SetDirection(tview.FlexRow).AddItem(searchBox, 2, 1, false).AddItem(mainBox, 0, 2, false)
+
+  searchBox.SetLabel("Search Service: ")
+  searchBox.SetDoneFunc(func(key tcell.Key){
+    app.Stop()
+  })
+
+  flex.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
     if event.Rune() == 113 {
       app.Stop()
     }
     return event
   })
 
-  if err := app.SetRoot(mainBox, true).EnableMouse(true).Run(); err != nil {
+  if err := app.SetRoot(flex, true).SetFocus(searchBox).EnableMouse(true).Run(); err != nil {
     panic(err)
   }
 }
